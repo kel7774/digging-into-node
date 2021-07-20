afcc113b391fba30452a4008c8a7353925078859
Kelli Landry <landry.kel@gmail.com>
2021-07-19 20:19:03 -0500

    low level for command line

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,2 +1,34 @@
-// My first Node.js program
-console.log('hello world')
+#!/usr/bin/env node
+'use strict'
+
+var args = require('minimist')(process.argv.slice(2), {
+  boolean: ['help'],
+  string: ['file']
+})
+
+if (args.help) {
+  printHelp()
+} else if (args.file) {
+  console.log(args.file)
+} else {
+  error('Incorrect usage', true)
+}
+
+printHelp()
+
+function error (message, includeHelp = false) {
+  console.error(message)
+  if (includeHelp) {
+    console.log('')
+    printHelp()
+  }
+}
+
+function printHelp () {
+  console.log('ex1 usage')
+  console.log('ex1.js --file={FILENAME}')
+  console.log('')
+  console.log('--help                print this help')
+  console.log('--file={FILENAME}     process the file')
+  console.log('')
+}
